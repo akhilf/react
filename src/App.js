b85c9bfdcb7fddb91709245dbc5d6f3225e217d6
akhilf@juniper.net <akhilf@juniper.net>
2018-11-02 17:15:16 +0530

    cards-app

--- src/App.js
+++ src/App.js
@@ -1,93 +1,64 @@
 import React, { Component } from 'react';
-import './Carousel.css';
-import Carousel from './compoents/Carousel';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './compoents/navbar';
+import Cards from './compoents/cards';
 
-// Data for carousel
-const Data = [
-  {
-    "id": 1,
-    "title": "We are Humans",
-    "subtitle": "And we love humans",
-    "text": "We act like humans, we talk like humans, and we think like humans. And we call out anyone who does the opposite.",
-    "image_url": "https://picsum.photos/300/150/?image=20",
-    "href": "https://mindera.com/people-and-culture/we-are-humans/",
-    "is_liked": true
-  },
-  {
-    "id": 2,
-    "title": "We work together",
-    "subtitle": "Would you like to join us?",
-    "text": "We insist on working collaborativelly. <strong>No rockstars</strong>. No departments. The whole owns the whole project together.",
-    "image_url": "https://picsum.photos/300/150/?image=30",
-    "href": "https://mindera.com/people-and-culture/we-work-together/",
-    "is_liked": false
-  },
-  {
-    "id": 3,
-    "title": "We change",
-    "subtitle": "And we embrace change",
-    "text": "Nothing is sacred, from our habits to our rituals, to our enviroment. Change is a natural part of the human life, and we prefer to embrace it.",
-    "image_url": "https://picsum.photos/300/150/?image=40",
-    "href": "https://mindera.com/people-and-culture/we-change/",
-    "is_liked": true
-  },
-  {
-    "id": 4,
-    "title": "We hire differently",
-    "text": "Most companies operate under the premise that employees should be replaceable like parts of an assembly line. We choose our people more carefully.",
-    "image_url": "https://picsum.photos/300/150/?image=50",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  },
-  {
-    "id": 5,
-    "title": "Get autonomous",
-    "text": "You’re given an incredible amount of freedom and autonomy at Mindera. That goes for everyone.",
-    "image_url": "https://picsum.photos/300/150/?image=60",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  },
-  {
-    "id": 6,
-    "title": "Work together",
-    "text": "Our flat structure calls for it by necessity. Being a leader may feel unnatural at first, but we expect everyone to step up and own part of the project.",
-    "image_url": "https://picsum.photos/300/150/?image=70",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  },
-  {
-    "id": 7,
-    "title": "Human Truths #1",
-    "subtitle": "From the book of humans",
-    "text": "Humans are not perfect. Don’t be afraid to fail. And when you do, you might as well fail spectacularly. This is how we grow and learn.",
-    "image_url": "https://picsum.photos/300/150/?image=80",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  },
-  {
-    "id": 8,
-    "title": "Human Truths #2",
-    "subtitle": "From the book of humans",
-    "text": "Humans are unique. Do you love Portugueses Pop Music (<em>Pimba</em>)? Do you prefer your desk covered with sunflowers? There’s no need to hide it. Be yourself. That’s how you’ll fit in here.",
-    "image_url": "https://picsum.photos/300/150/?image=90",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  },
-  {
-    "id": 9,
-    "title": "Humans are not \"resources\"",
-    "subtitle": "Humans are humans",
-    "text": "We don’t have an HR department. New hires are interviewed by the people who will be working with them. So get ready to care a lot about the people you work with.",
-    "image_url": "https://picsum.photos/300/150/?image=91",
-    "href": "https://mindera.com/people-and-culture/faq/",
-    "is_liked": false
-  }
-];
 class App extends Component {
+  state = {
+    items : [
+      {id:1, value: 0},
+      {id:2, value: 0},
+      {id:3, value: 0},
+      {id:4, value: 0},
+      {id:5, value: 0},
+      {id:6, value: 0},
+      {id:7, value: 0},
+      {id:8, value: 0},
+      {id:9, value: 0},
+      {id:10, value: 0},
+    ]
+  };
+  constructor() {
+    super();
+    console.log("App - constructor");
+  }
+  compoentDidMount() {
+    //Ajax calll
+    // setState
+    console.log('App - Mounted');
+  }
+  handleReset = () => {
+    const items = this.state.items.map(c=> {
+      c.value = 0;
+      return c;
+    });
+    this.setState({items});
+  };
+  handleIncrment = item => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index].value++;
+    this.setState({items});
+  }
+  handleDelete = itemsId => {
+    const items = this.state.items.filter(c => c.id !== itemsId);
+    this.setState({items});
+  }
   render() {
+    console.log('App rendered');
     return (
-      <div><Carousel slides={Data} /></div>
+      <React.Fragment>
+        <Navbar totalNumbers={this.state.items.filter(c => c.value > 0).length}/>
+        <main className="container">
+          <Cards items={this.state.items}
+                    onReset={this.handleReset}
+                    onIncrement={this.handleIncrment}
+                    onDelete={this.handleDelete}/>
+        </main>
+      </React.Fragment>
     );
   }
 }
+
 export default App;
